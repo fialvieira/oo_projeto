0f64f6be171d5db6791f6a4338c1a0191eae7d4f
81102569 <filipevieira@correios.com.br>
2025-08-21 10:13:55 -0300

    Modificador de propriedade e métodos get e set.

--- modelos/biblioteca.py
+++ modelos/biblioteca.py
@@ -1,16 +1,33 @@
 # Classe Biblioteca
 class Biblioteca:
+    bibliotecas = []
+
     def __init__(self, nome="", ativo=False) -> None:
         self.nome = nome
-        self.ativo = ativo
+        self._ativo = ativo # Atributo privado, com _propriedade
+        Biblioteca.bibliotecas.append(self)
 
     def __str__(self) -> str:
-        return f"Biblioteca: {self.nome}\nAtivo: {"Sim" if self.ativo else "Não"}"
+        return f"Biblioteca: {self.nome}\nAtivo: {"Sim" if self._ativo else "Não"}"
+
+    def listar_bibliotecas():
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome} | {biblioteca.ativo}")
+    
+    # Método set  
+    def alterna_estado(self):
+        self._ativo = not self._ativo
+    
+    # Método get
+    @property
+    def ativo(self):
+        return "ativada" if self._ativo else "desativada"
+    
+
 
 biblioteca_cidade = Biblioteca("Biblioteca Municipal", True)
+biblioteca_cidade.alterna_estado()
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping", False)
+biblioteca_shopping.alterna_estado()
 
-bibliotecas = [biblioteca_cidade, biblioteca_shopping]
-
-for biblioteca in bibliotecas:
-    print(biblioteca)
+Biblioteca.listar_bibliotecas()
